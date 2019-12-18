f25522b239c73df8defb84d8f294f0930f5c4c91
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-12-19 01:12:45 +0530

    name removed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hemant</h1>
         <Lottery />
         <Lottery title='Mini Daily' maxNum={10} maxBalls={4} />
       </div>
